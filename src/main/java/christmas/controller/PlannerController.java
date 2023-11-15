85d869dfc98a55b61d1b88c2a6734e600aa478a9
PeterChoi <94peterchoi@gmail.com>
2023-11-15 18:47:22 +0900

    feat: 구현된 기능을 바탕으로 프로그램 제어흐름 일부분 작성
    - 안내메세지 노출 후 사용자로부터 방문날짜와 주문에 대한 정보를 입력받는다

--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -1,8 +1,18 @@
 package christmas.controller;
 
+import christmas.domain.Orders;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.time.LocalDate;
+
 public class PlannerController {
 
     public void run() {
+        OutputView.printGreeting();
+
+        LocalDate reservationDate = InputView.getReservationDate();
+        Orders orders = InputView.getClientOrders();
 
     }
 
