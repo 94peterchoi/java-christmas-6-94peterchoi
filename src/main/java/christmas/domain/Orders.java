fc2efe77000f831388a09b63865969349db25b43
PeterChoi <94peterchoi@gmail.com>
2023-11-15 18:25:06 +0900

    feat: 여러 개의 주문을 관리하는 Orders 도메인 클래스 추가
    - 메뉴의 중복이 있는 경우, 총 주문 개수가 20개를 초과할 시, 혹은 음료만 주문된 경우 예외발생

---
+++ src/main/java/christmas/domain/Orders.java
@@ -0,0 +1,53 @@
+package christmas.domain;
+
+import christmas.exception.InvalidOrderException;
+
+import java.util.List;
+
+public class Orders {
+
+    private static final int REQUIRED_FOOD_ORDER_COUNT = 1;
+    private static final int MAXIMUM_ORDER_COUNT = 20;
+
+    private final List<Order> orders;
+
+    public Orders(List<Order> orders) {
+        validateOrders(orders);
+        this.orders = orders;
+    }
+
+    private void validateOrders(List<Order> orders) {
+        if (getCountOfOrdersWithoutDuplicates(orders) != orders.size()) {
+            throw new InvalidOrderException();
+        }
+
+        if (getCountOfAllOrdersExceptSpecificMenu(orders, MenuType.DRINK) < REQUIRED_FOOD_ORDER_COUNT) {
+            throw new InvalidOrderException();
+        }
+
+        if (getCountOfAllOrders(orders) > MAXIMUM_ORDER_COUNT) {
+            throw new InvalidOrderException();
+        }
+    }
+
+    public int getCountOfOrdersWithoutDuplicates(List<Order> orders) {
+        return (int) orders.stream()
+                .map(Order::getMenu)
+                .distinct()
+                .count();
+    }
+
+    public int getCountOfAllOrders(List<Order> orders) {
+        return orders.stream()
+                .mapToInt(Order::getCount)
+                .sum();
+    }
+
+    public int getCountOfAllOrdersExceptSpecificMenu(List<Order> orders, MenuType menuType) {
+        return orders.stream()
+                .filter(order -> !order.isMenuTypeOf(menuType))
+                .mapToInt(Order::getCount)
+                .sum();
+    }
+
+}
