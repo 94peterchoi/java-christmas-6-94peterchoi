f76877771c7b1888f3e56967edd18eb04753c175
PeterChoi <94peterchoi@gmail.com>
2023-11-15 21:40:38 +0900

    feat: 서비스코드에 대한 의존성 주입 및 프로그램 제어 흐름 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,11 @@
 package christmas;
 
 import christmas.controller.PlannerController;
+import christmas.service.PlannerService;
 
 public class Application {
     public static void main(String[] args) {
-        PlannerController plannerController = new PlannerController();
+        PlannerController plannerController = new PlannerController(new PlannerService());
         plannerController.run();
     }
 }
--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -1,6 +1,9 @@
 package christmas.controller;
 
+import christmas.domain.Badge;
+import christmas.domain.Benefits;
 import christmas.domain.Orders;
+import christmas.service.PlannerService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -8,12 +11,21 @@ import java.time.LocalDate;
 
 public class PlannerController {
 
+    private final PlannerService plannerService;
+
+    public PlannerController(PlannerService plannerService) {
+        this.plannerService = plannerService;
+    }
+
     public void run() {
         OutputView.printGreeting();
 
         LocalDate reservationDate = InputView.getReservationDate();
         Orders orders = InputView.getClientOrders();
+        Benefits benefits = plannerService.generateBenefits(orders, reservationDate);
+        Badge badge = plannerService.determineBadge(benefits);
 
+        displayResults();
     }
 
 }
