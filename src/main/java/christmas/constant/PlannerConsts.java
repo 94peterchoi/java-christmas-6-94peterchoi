411e8a691d364c304519bdc0919fab845155af00
PeterChoi <94peterchoi@gmail.com>
2023-11-15 17:50:16 +0900

    feat: 공통으로 사용되는 상수들을 관리해주는 클래스 추가

---
+++ src/main/java/christmas/constant/PlannerConsts.java
@@ -0,0 +1,9 @@
+package christmas.constant;
+
+public class PlannerConsts {
+
+    public static final int YEAR_OF_PLANNER = 2023;
+    public static final int MONTH_OF_PLANNER = 12;
+    public static final int CHRISTMAS_DAY = 25;
+
+}
