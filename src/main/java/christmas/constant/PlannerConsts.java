0033a9577235c8643fb7ea3c16ee799dec58e563
PeterChoi <94peterchoi@gmail.com>
2023-11-15 21:58:58 +0900

    feat: 단위 출력에 필요한 문자열 상수 정의

--- src/main/java/christmas/constant/PlannerConsts.java
+++ src/main/java/christmas/constant/PlannerConsts.java
@@ -2,6 +2,9 @@ package christmas.constant;
 
 public class PlannerConsts {
 
+    public static final String MONEY_UNIT = "원";
+    public static final String ITEM_UNIT = "개";
+
     public static final int YEAR_OF_PLANNER = 2023;
     public static final int MONTH_OF_PLANNER = 12;
     public static final int CHRISTMAS_DAY = 25;
